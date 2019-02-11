0e3f884803f5c2bbe5751cae19ce2eb08d71c1c6
visavi <visavi.net@mail.ru>
2019-02-11 18:42:51 +0300

    Модуль стикеры

---
+++ Sticker/migrations/20190211165911_append_to_stickers.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AppendToStickers extends AbstractMigration
+{
+    /**
+     * Migrate Up.
+     */
+    public function up(): void
+    {
+        $categories = array_map('basename', glob(APP . '/Modules/Sticker/images/*', GLOB_ONLYDIR));
+
+        foreach ($categories as $categoryName) {
+            $this->execute("INSERT INTO stickers_categories (name, created_at) VALUES ('" . $categoryName . "', " . SITETIME .");");
+            $lastId = $this->getAdapter()->getConnection()->lastInsertId();
+
+            $stickers = array_map('basename', glob(APP . '/Modules/Sticker/images/' . $categoryName . '/*.{gif,png,jpg,jpeg}', GLOB_BRACE));
+
+            foreach ($stickers as $stickerName) {
+                $this->execute("INSERT INTO stickers (category_id, name, code) VALUES (" . $lastId . ", '/assets/modules/stickers/" . $categoryName . "/" . $stickerName . "', ':" . getBodyName($stickerName) . "');");
+            }
+        }
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down(): void
+    {
+        $categories = array_map('basename', glob(APP . '/Modules/Sticker/images/*', GLOB_ONLYDIR));
+
+        foreach ($categories as $categoryName) {
+
+            $category = $this->fetchRow('SELECT id FROM stickers_categories WHERE name = "' . $categoryName . '" LIMIT 1;');
+
+            if ($category) {
+                $this->execute("DELETE FROM stickers_categories WHERE id='" . $category['id'] . "';");
+                $this->execute("DELETE FROM stickers WHERE category_id='" . $category['id'] . "';");
+            }
+        }
+    }
+}
---
+++ Sticker/module.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name'        => 'Стикеры',
+    'description' => 'Комплект дополнительных стикеров для сайта',
+    'version'     => '1.0',
+    'author'      => 'Vantuz',
+    'email'       => 'admin@visavi.net',
+    'homepage'    => 'https://visavi.net',
+    'symlinks'    => [
+        'images' => HOME . '/assets/modules/stickers',
+    ],
+];
