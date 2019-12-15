de156c2e606a0af8931a281460bb09ac6c1b940b
visavi <visavi.net@mail.ru>
2019-12-16 00:28:48 +0300

    fix sticker module

--- Sticker/migrations/20190211165911_append_to_stickers.php
+++ Sticker/migrations/20190211165911_append_to_stickers.php
@@ -9,13 +9,12 @@ class AppendToStickers extends AbstractMigration
      */
     public function up(): void
     {
-        $categories = array_map('basename', glob(MODULES . '/Sticker/images/*', GLOB_ONLYDIR));
-
+        $categories = array_map('basename', glob(MODULES . '/Sticker/resources/assets/*', GLOB_ONLYDIR));
         foreach ($categories as $categoryName) {
             $this->execute("INSERT INTO stickers_categories (name, created_at) VALUES ('" . $categoryName . "', " . SITETIME .");");
             $lastId = $this->getAdapter()->getConnection()->lastInsertId();
 
-            $stickers = array_map('basename', glob(MODULES . '/Sticker/images/' . $categoryName . '/*.{gif,png,jpg,jpeg}', GLOB_BRACE));
+            $stickers = array_map('basename', glob(MODULES . '/Sticker/resources/assets/' . $categoryName . '/*.{gif,png,jpg,jpeg}', GLOB_BRACE));
 
             foreach ($stickers as $stickerName) {
                 $this->execute("INSERT INTO stickers (category_id, name, code) VALUES (" . $lastId . ", '/assets/modules/stickers/" . $categoryName . "/" . $stickerName . "', ':" . getBodyName($stickerName) . "');");
@@ -30,10 +29,9 @@ class AppendToStickers extends AbstractMigration
      */
     public function down(): void
     {
-        $categories = array_map('basename', glob(MODULES . '/Sticker/images/*', GLOB_ONLYDIR));
+        $categories = array_map('basename', glob(MODULES . '/Sticker/resources/assets/*', GLOB_ONLYDIR));
 
         foreach ($categories as $categoryName) {
-
             $category = $this->fetchRow('SELECT id FROM stickers_categories WHERE name = "' . $categoryName . '" LIMIT 1;');
 
             if ($category) {
--- Sticker/module.php
+++ Sticker/module.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Стикеры',
     'description' => 'Комплект дополнительных стикеров для сайта',
-    'version'     => '1.0',
+    'version'     => '1.1',
     'author'      => 'Vantuz',
     'email'       => 'admin@visavi.net',
     'homepage'    => 'https://visavi.net',
