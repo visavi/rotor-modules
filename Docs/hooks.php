18a97cc09422058d523d5e9bc15c9d10410c58ca
visavi <visavi.net@mail.ru>
2025-05-31 12:55:37 +0300

    Изменены стили хуков для новой версии

--- Docs/hooks.php
+++ Docs/hooks.php
@@ -11,7 +11,7 @@ Hook::add('sidebarMenuEnd', function ($content) {
         <a class="app-menu__item' . (request()->is('rotor*') ? ' active' : '') . '" href="/rotor">
             <i class="app-menu__icon fa-solid fa-gear"></i>
             <span class="app-menu__label text-danger">RotorCMS</span>
-            <span class="badge bg-dark bg-gradient">' . $version . '</span>
+            <span class="badge bg-sidebar">' . $version . '</span>
         </a>
     </li>' . PHP_EOL;
 });
