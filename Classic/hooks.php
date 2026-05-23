812d977596196b5844c0b542194f98ca28716c6a
visavi <visavi.net@mail.ru>
2026-05-24 01:54:20 +0300

    Убрал лишние вызовы в Classic

--- Classic/hooks.php
+++ Classic/hooks.php
@@ -2,7 +2,4 @@
 
 use App\Classes\Hook;
 
-view()->addLocation(base_path('modules/Classic/resources/views'));
-app('translator')->addNamespace('classic', base_path('modules/Classic/resources/lang'));
-
 Hook::add('homepageView', static fn () => view('classic::widgets/_classic')->render());
